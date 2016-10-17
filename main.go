351b5b74cf61dad0c476266863e2d0e13444b077
Mark St.Godard <markstgodard@gmail.com>
2016-10-17 09:40:19 -0500

    Add reviews

--- main.go
+++ main.go
@@ -16,8 +16,14 @@ type Product struct {
 	Category     string  `json:"category"`
 	ThumbnailURL string  `json:"thumbnail_url"`
 	Description  string  `json:"description"`
-	Stars        int     `json:"stars"`
+	Review       Review  `json:"review"`
 	Reviews      int     `json:"reviews"`
+}
+
+type Review struct {
+	ProductID   int    `json:"product_id"`
+	Description string `json:"description"`
+	Stars       int    `json:"stars"`
 }
 
 var products []Product
@@ -33,15 +39,59 @@ func init() {
 }
 
 func getProducts(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(products)
+	log.Println("fetching products..")
+	data := make([]Product, len(products))
+	copy(data, products)
+
+	// get reviews
+	reviews := getReviews()
+	for i, _ := range data {
+		for k, _ := range reviews {
+			if data[i].ID == reviews[k].ProductID {
+				data[i].Review = reviews[k]
+				data[i].Reviews += 1
+			}
+		}
+	}
+	json.NewEncoder(w).Encode(data)
+}
+
+func getReviews() []Review {
+	log.Println("fetching reviews..")
+	res, err := http.Get(reviewsAPI)
+	if err != nil {
+		log.Printf("Error fetching reviews: %s\n", err.Error())
+		return []Review{}
+	}
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Printf("Error reading response body for reviews: %s\n", err.Error())
+		return []Review{}
+	}
+
+	var reviews []Review
+	err = json.Unmarshal(data, &reviews)
+	if err != nil {
+		log.Printf("Error unmarshaling reviews: %s\n", err.Error())
+		return []Review{}
+	}
+	return reviews
 }
 
 func health(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
+var reviewsAPI string
+
 func main() {
 	mux := http.NewServeMux()
+
+	reviewsAddr := os.Getenv("REVIEWS_PROXY_ADDR")
+	reviewsAPI = fmt.Sprintf("%s/api/reviews", reviewsAddr)
+	log.Printf("reviews api: %s\n", reviewsAPI)
 
 	mux.HandleFunc("/health", health)
 	mux.HandleFunc("/api/products", getProducts)
