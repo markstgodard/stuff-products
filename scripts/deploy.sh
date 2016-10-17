02efa3d134166c50c96800b6d62277a439a455ce
Mark St.Godard <markstgodard@gmail.com>
2016-10-17 09:53:08 -0500

    v1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source $(pwd)/scripts/cf.cfg
 
-cf push ${APP_NAME} -o markstgodard/stuff-products:v0 --no-start --no-manifest --no-route
-cf set-env ${APP_NAME} A8_SERVICE "products:v0"
+cf push ${APP_NAME} -o markstgodard/stuff-products:v1 --no-start --no-manifest --no-route
+cf set-env ${APP_NAME} A8_SERVICE "products:v1"
 cf set-env ${APP_NAME} A8_ENDPOINT_PORT "8080"
 cf set-env ${APP_NAME} A8_ENDPOINT_TYPE "http"
 cf set-env ${APP_NAME} A8_PROXY "true"
